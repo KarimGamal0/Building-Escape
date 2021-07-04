5d43b590e0fc36d7a87e6cc6b78a860d56e79615
Karim-PC <kgamalali@gmail.com>
2021-07-04 22:30:31 +0200

    Adding Frame Door

---
+++ Source/Building_Escape/OpenDoor.cpp
@@ -0,0 +1,40 @@
+// Karim Gamal Copyright
+
+
+#include "OpenDoor.h"
+#include "Actor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//FRotator CurrentRotation = GetOwner()->GetActorRotation();
+
+	//CurrentRotation.Yaw = 90.0f;
+
+	FRotator OpenDoor = { 0.0f,90.0f,0.0f };
+
+	GetOwner()->SetActorRotation(OpenDoor);
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
